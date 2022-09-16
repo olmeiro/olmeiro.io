64f64654a2631e911e5fb5d0d98c05e21da8a4a4
Olmeiro orozco <olme.orozco@gmail.com>
2022-09-15 23:02:53 -0500

    dark mode set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
+import { useEffect, useState } from 'react'
 
 function App () {
+  const [theme, setTheme] = useState(null)
+
+  useEffect(() => {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      setTheme('dark')
+    } else {
+      setTheme('light')
+    }
+  }, [])
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+  }, [theme])
+
+  const handleThemeSwitch = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark')
+  }
+
   return (
-    <div className="min-h-screen bg-slate-300">
-      <h1 className='bg-slate-500'>App</h1>
-    </div>
+    <>
+      <button
+        type='button'
+        onClick={handleThemeSwitch}
+        className="fixed z-10 right-2 top-8 bg-indigo-500 text-lg p-1 rounded-md"
+      >
+        { theme === 'dark' ? '🌑' : '☀️'}
+      </button>
+      <div className='bg-red dark:bg-slate-900'>
+          <h1 className='dark:text-white'>Olmeiro portfolio</h1>
+      </div>
+    </>
   )
 }
 
