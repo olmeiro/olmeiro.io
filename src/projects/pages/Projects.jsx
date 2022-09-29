b1fa1174ddbd0655cdfb82f155fb709b9f254a6f
Olmeiro orozco <olme.orozco@gmail.com>
2022-09-29 12:09:05 -0500

    fix: ajuste medidas card proyectos

--- src/projects/pages/Projects.jsx
+++ src/projects/pages/Projects.jsx
@@ -1,4 +1,5 @@
 import { FaGithub } from 'react-icons/fa'
+import { MdDateRange } from 'react-icons/md'
 import { Layout } from '../../layout/Layout'
 import { projects } from '../data'
 
@@ -6,7 +7,7 @@ export const Projects = () => {
   return (
     <Layout>
       <div className="flex flex-col justify-center">
-        <h2 className="text-4xl md:text-5xl text-center  font-advent font-extrabold leading-snug">
+        <h2 className="text-4xl md:text-5xl text-center p-4  font-advent font-extrabold leading-snug">
           Projects: {projects.length}
         </h2>
         <section className="flex flex-col justify-center items-center gap-4 flex-wrap md:flex-row mt-3">
@@ -14,28 +15,28 @@ export const Projects = () => {
             return (
               <div
                 key={idx + 1}
-                className="w-72 md:w-96 h-auto md:h-1/2 mb-3 border border-solid divide-black  outline-2 rounded-lg shadow-xl"
+                className="w-72 md:w-72 h-auto md:h-1/2 mb-3 border border-solid divide-black  outline-2 rounded-lg shadow-xl"
               >
                 <img
-                  className="rounded-t-md w-72 h-44 md:w-full md:h-72"
+                  className="rounded-t-md w-72 h-44 "
                   src={project.image}
                   alt="image project"
                 />
                 <div>
-                  <h2 className="flex justify-center items-center h-20 text-2xl md:text-4xl text-center m-3 font-advent font-extrabold leading-snug">
+                  <h2 className="flex justify-center items-center h-12 mt-3 p-4 text-xl md:text-xl text-center font-dmsans font-bold leading-snug">
                     {project.title}
                   </h2>
-                  <p className="flex justify-end text-xs font-dmsans font-semibold m-3">
-                    {project.date}
+                  <p className="flex justify-end font-dmsans text-xs m-3">
+                    <i>{<MdDateRange/>}</i>{project.date}
                   </p>
                 </div>
-                <p className="h-44 font-dmsans m-3 subpixel-antialiased tracking-wider leading-relaxed text-center md:text-left">
+                <p className="h-28 font-dmsans text-xs m-4 subpixel-antialiased tracking-wider leading-relaxed text-center">
                   {project.description}
                 </p>
                 <section className="h-32">
                   <div className="flex flex-row justify-center gap-3 flex-wrap">
                     <h3 className="font-semibold">Technologies:</h3>
-                    <div className=" flex justify-center gap-4 flex-wrap">
+                    <div className=" flex justify-center gap- flex-wrap">
                       {project.tecnologies.map((tech, idx) => {
                         return (
                           <span
@@ -49,7 +50,7 @@ export const Projects = () => {
                     </div>
                   </div>
                 </section>
-                <p className="text-sm font-advent font-semibold m-3 flex items-center justify-end gap-3">
+                <p className="text-base font-advent font-semibold m-3 flex items-center justify-end gap-3">
                   Repo:
                   <a href={project.repo} target="_blank" rel="noreferrer" className=''><FaGithub /></a>
                 </p>
