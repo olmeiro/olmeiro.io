a836214ff052748ea6e857a390faf7b427912dba
Olmeiro orozco <olme.orozco@gmail.com>
2022-09-29 09:20:10 -0500

    feat: hook formulario contacto

---
+++ src/contact/hooks/useForm.js
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+export const useForm = (initialForm = {}) => {
+  const [formState, setFormState] = useState(initialForm)
+
+  const hadleInputChange = ({ target }) => {
+    const { name, value } = target
+    setFormState({
+      ...formState,
+      [name]: value
+    })
+  }
+
+  return {
+    ...formState,
+    formState,
+    hadleInputChange
+  }
+}
